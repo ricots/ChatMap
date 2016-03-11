a0f8d47ca447f71e0d0b8c8826c904caef80c5ae
Brian O'Neill <hello@brianoneill.im>
2016-03-11 15:27:54 +0000

    #0002 add camera icon

---
+++ app/src/main/java/im/brianoneill/chatmap/controller/IdCreatorActivity.java
@@ -0,0 +1,15 @@
+package im.brianoneill.chatmap.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import im.brianoneill.chatmap.R;
+
+public class IdCreatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_id_creator);
+    }
+}
