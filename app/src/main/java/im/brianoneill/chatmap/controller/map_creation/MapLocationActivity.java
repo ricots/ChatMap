c989ef4d1f3429978e053a51ca8908acdd0bdd96
Brian O'Neill <hello@brianoneill.im>
2016-03-13 16:52:42 +0000

    #0006 add map creator and location xml

---
+++ app/src/main/java/im/brianoneill/chatmap/controller/map_creation/MapLocationActivity.java
@@ -0,0 +1,15 @@
+package im.brianoneill.chatmap.controller.map_creation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import im.brianoneill.chatmap.R;
+
+public class MapLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_location);
+    }
+}
